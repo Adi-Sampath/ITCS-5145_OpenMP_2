b8f3b53fdcd50dfb69f2324884fdc30fd796f306
Adi-Sampath <asampat1@uncc.edu>
2023-10-24 14:53:55 -0500

    commit

--- mergesort-loop/mergesort-loop.cpp
+++ mergesort-loop/mergesort-loop.cpp
@@ -1,23 +1,18 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <iostream>
-#include <unistd.h>
 #include <omp.h>
 #include <chrono>
+#include <cstdlib>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  void generateMergeSortData (int* arr, size_t n);
-  void checkMergeSortResult (int* arr, size_t n);
+void generateMergeSortData(int *arr, size_t n);
+void checkMergeSortResult(int *arr, size_t n);
 #ifdef __cplusplus
 }
 #endif
 
-void merge(int * arr, int l[], int r[], int l_size, int r_size) {
-
+void merge(int *arr, int l[], int r[], int l_size, int r_size) {
   int i = 0, j = 0, k = 0;
 
   while (i < l_size && j < r_size) {
@@ -25,27 +20,26 @@ void merge(int * arr, int l[], int r[], int l_size, int r_size) {
       arr[k] = l[i];
       i++;
     } else {
-        arr[k] = r[j];
-        j++;
-      }
-      k++;
-    }
-  // Copy any remaining elements from l and r (if any)
-  while (i < l_size) {
-      arr[k] = l[i];
-      i++;
-      k++;
-  }
-  while (j < r_size) {
       arr[k] = r[j];
       j++;
-      k++;
+    }
+    k++;
+  }
+
+  while (i < l_size) {
+    arr[k] = l[i];
+    i++;
+    k++;
+  }
+  while (j < r_size) {
+    arr[k] = r[j];
+    j++;
+    k++;
   }
 }
 
 void parallelMergeSort(int arr[], int n) {
-
-  if(n > 1) {
+  if (n > 1) {
     int mid = n / 2;
     int *left = arr;
     int *right = arr + mid;
@@ -62,26 +56,13 @@ void parallelMergeSort(int arr[], int n) {
       }
     }
 
-    // Merge the two sorted subarrays (sequentially)
     merge(arr, left, right, mid, n - mid);
   }
 }
 
-int main (int argc, char* argv[]) {
-
-  //forces openmp to create the threads beforehand
-#pragma omp parallel
-  {
-    int fd = open (argv[0], O_RDONLY);
-    if (fd != -1) {
-      close (fd);
-    }
-    else {
-      std::cerr<<"something is amiss"<<std::endl;
-    }
-  }
-
-  if (argc < 3) { std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " <n> <nbthreads>" << std::endl;
     return -1;
   }
 
@@ -89,24 +70,22 @@ int main (int argc, char* argv[]) {
   int numThreads = atoi(argv[2]);
 
   // get arr data
-  int * arr = new int [n];
-  generateMergeSortData (arr, n);
+  int *arr = new int[n];
+  generateMergeSortData(arr, n);
 
-
-  //insert sorting code here.
   // start timing
   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
   omp_set_num_threads(numThreads);
   parallelMergeSort(arr, n);
-  //printArray(arr, n);
+
   // end time
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapased_seconds = end-start;
-  //Print the total execution time (in sec) to the error stream
-  std::cerr<<elapased_seconds.count()<<std::endl;
-  
-  checkMergeSortResult (arr, n);
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  // Print the total execution time (in sec) to the error stream
+  std::cerr << elapsed_seconds.count() << std::endl;
+
+  checkMergeSortResult(arr, n);
 
   delete[] arr;
 
