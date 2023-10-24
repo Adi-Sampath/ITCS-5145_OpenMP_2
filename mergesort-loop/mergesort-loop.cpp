58b9c7f7d922a207b666b57bfb00f3c145d373b4
Adi-Sampath <asampat1@uncc.edu>
2023-10-24 12:54:48 -0500

    commit merge sort

--- mergesort-loop/mergesort-loop.cpp
+++ mergesort-loop/mergesort-loop.cpp
@@ -16,6 +16,38 @@ extern "C" {
 }
 #endif
 
+void merge(int* arr, int l, int m, int r) {
+  int i, j, k;
+  int n1 = m - l + 1; // size of left subarray
+  int n2 =  r - m; // size of right subarray
+
+  /* create temp arrays */
+  int* L = new int[n1];
+  int* R = new int[n2];
+
+  /* Copy data to temp arrays L[] and R[] */
+  for (i = 0; i < n1; i++) {
+    L[i] = arr[l + i];
+  }
+  for (j = 0; j < n2; j++) {
+    R[j] = arr[m + 1+ j];
+  }
+
+  /* Merge the temp arrays back into arr[l..r]*/
+  i = 0; // Initial index of first subarray
+  j = 0; // Initial index of second subarray
+  k = l; // Initial index of merged subarray
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k++] = L[i++];
+    }
+    else {
+      arr[k++] = R[j++];
+    }
+  }
+
+}
+
 int main (int argc, char* argv[]) {
 
   //forces openmp to create the threads beforehand
@@ -46,13 +78,28 @@ int main (int argc, char* argv[]) {
   // start timing
   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
+  // parallel merge sort
+  int i, j;
+  int chunk = n / numThreads;
+  int left, mid, right;
+  int tid;
+  #pragma omp for schedule(static, chunk) private(i, j, left, mid, right, tid)
+  for (i = 1; i <= n - 1; i = 2 * i) {
+    for (j = 0; j < n - 1; j += 2 * i) {
+      left = j;
+      mid = j + i - 1;
+      right = std::min(j + 2 * i - 1, n - 1);
+      merge(arr, left, mid, right);
+    }
+  }
+
 
   //printArray(arr, n);
   // end time
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapased_seconds = end-start;
   //Print the total execution time (in sec) to the error stream
-  cerr<<elapased_seconds.count()<<std::endl;
+  std::cerr<<elapased_seconds.count()<<std::endl;
   
   checkMergeSortResult (arr, n);
 
