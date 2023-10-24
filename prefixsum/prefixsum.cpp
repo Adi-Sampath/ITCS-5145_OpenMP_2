a2fdaf30d24536dacd4f440f588a5ed378bcdbd7
Adi-Sampath <asampat1@uncc.edu>
2023-10-23 21:15:59 -0500

    commit

--- prefixsum/prefixsum.cpp
+++ prefixsum/prefixsum.cpp
@@ -67,7 +67,7 @@ int main (int argc, char* argv[]) {
     }
     for(int i = start; i < end; i++){
       partial_sum += arr[i];
-      std::cout << " Element " << i << " is " << arr[i] << std::endl;
+      std::cout << "Element " << i << " is " << arr[i] << std::endl;
       std::cout << "Partial Sum is " << partial_sum << std::endl;
       pr[i] = partial_sum;
     }
