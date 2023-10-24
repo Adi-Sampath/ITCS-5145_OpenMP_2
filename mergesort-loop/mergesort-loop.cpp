3009583991fb8addbe8e0b6e59fc2e33ef4815b5
Adi-Sampath <asampat1@uncc.edu>
2023-10-24 14:24:46 -0500

    commit

--- mergesort-loop/mergesort-loop.cpp
+++ mergesort-loop/mergesort-loop.cpp
@@ -16,36 +16,55 @@ extern "C" {
 }
 #endif
 
-void merge(int* arr, int l, int m, int r) {
-  int i, j, k;
-  int n1 = m - l + 1; // size of left subarray
-  int n2 =  r - m; // size of right subarray
+void merge(int * arr, int l[], int r[], int l_size, int r_size) {
 
-  /* create temp arrays */
-  int* L = new int[n1];
-  int* R = new int[n2];
+  int i = 0, j = 0, k = 0;
 
-  /* Copy data to temp arrays L[] and R[] */
-  for (i = 0; i < n1; i++) {
-    L[i] = arr[l + i];
-  }
-  for (j = 0; j < n2; j++) {
-    R[j] = arr[m + 1+ j];
-  }
-
-  /* Merge the temp arrays back into arr[l..r]*/
-  i = 0; // Initial index of first subarray
-  j = 0; // Initial index of second subarray
-  k = l; // Initial index of merged subarray
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k++] = L[i++];
+  while (i < l_size && j < r_size) {
+    if (l[i] <= r[j]) {
+      arr[k] = l[i];
+      i++;
+    } else {
+        arr[k] = r[j];
+        j++;
+      }
+      k++;
     }
-    else {
-      arr[k++] = R[j++];
-    }
+  // Copy any remaining elements from l and r (if any)
+  while (i < l_size) {
+      arr[k] = l[i];
+      i++;
+      k++;
   }
+  while (j < r_size) {
+      arr[k] = r[j];
+      j++;
+      k++;
+  }
+}
 
+void parallelMergeSort(int arr[], int n) {
+
+  if(n > 1) {
+    int mid = n / 2;
+    int *left = arr;
+    int *right = arr + mid;
+
+    #pragma omp parallel sections
+    {
+      #pragma omp section
+      {
+        parallelMergeSort(left, mid);
+      }
+      #pragma omp section
+      {
+        parallelMergeSort(right, n - mid);
+      }
+    }
+
+    // Merge the two sorted subarrays (sequentially)
+    merge(arr, left, right, mid, n - mid);
+  }
 }
 
 int main (int argc, char* argv[]) {
@@ -78,22 +97,8 @@ int main (int argc, char* argv[]) {
   // start timing
   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
-  // parallel merge sort
-  int i, j;
-  int chunk = n / numThreads;
-  int left, mid, right;
-  int tid;
-  #pragma omp for schedule(static, chunk) private(i, j, left, mid, right, tid)
-  for (i = 1; i <= n - 1; i *= 2) {
-    for (j = 0; j < n - 1; j += (2 * i)) {
-      left = j;
-      mid = j + i - 1;
-      right = std::min(j + 2 * i - 1, n - 1);
-      merge(arr, left, mid, right);
-    }
-  }
-
-
+  omp_set_num_threads(numThreads);
+  parallelMergeSort(arr, n);
   //printArray(arr, n);
   // end time
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
